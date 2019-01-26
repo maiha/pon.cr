b67c52dbc49a5f4d67b81767389289663d93228e
maiha <maiha@wota.jp>
2019-01-27 07:23:55 +0900

    Model#to_h

--- spec/core_spec.cr
+++ spec/core_spec.cr
@@ -18,6 +18,16 @@ module {{adapter.upcase.id}}
         job.to_s.sub(/[A-Z]+::/,"").should eq("#<Job id: 1, name: \"foo\", time: nil>")
       end
     end
+
+    describe "Model#to_h" do
+      it "converts to a Hash" do
+        job = Job.new(name: "foo")
+        job.to_h.should eq({"id" => nil, "name" => "foo", "time" => nil})
+
+        job = Job.new(id: 1, name: "foo")
+        job.to_h.should eq({"id" => 1, "name" => "foo", "time" => nil})
+      end
+    end
   end
 end
 {% end %}
